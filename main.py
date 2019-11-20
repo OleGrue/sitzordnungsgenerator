e62cc274af95e83b5cb7b1bf80f9a61d9b76d353
OleGrue <49246995+OleGrue@users.noreply.github.com>
2019-11-20 21:20:18 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import pdfkit
+import re
 from selenium import webdriver
 
 
@@ -24,12 +26,6 @@ class generateRandomSeating:
 		
 		print(result)
 		return result
-	
-	def structured_output():
-		pass
-	
-	def feedback():
-		pass
 
 
 def post_to_whatsapp(message):
@@ -53,8 +49,24 @@ def post_to_whatsapp(message):
 	    button.click()
 
 
+def generate_pdf(array_people):
 
-array_people = ['Ann Kathrin', 'Ben', 'Charlotte', 'Dana', 'Emily', 'Erik', 'Henrike', 'Jannis', 'John', 'Jonas', 'Julius', 'Martin', 'Mieke', 'Niklas', 'Niko', 'Ole', 'Oliver', 'Robin', 'Sam', 'Thore', 'Tim Ole', 'Volker']
+	count = 1
+	page = '<!DOCTYPE html> <html lang="de"> <head> <meta charset="utf-8" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <title>Titel</title> <style type="text/css"> .platz{ background-color:#797979; color:#ffff; text-align:center; } html{ font-size: 200%; } </style> </head> <body> <div> <table> <tr> <th id="11" class="platz">p11p</th> <th id="10"class="platz">p10p</th> <th id="9"class="platz">p9p</th> <th></th> <th id="8"class="platz">p8p</th> <th id="7"class="platz">p7p</th> <th id="6"class="platz">p6p</th> <td>|</td> </tr> <tr> <td id="12"class="platz">p12p</td> <td></td> <td></td> <td></td> <td></td> <td></td> <td id="5"class="platz">p5p</td> <td>|</td> </tr> <tr> <td id="13"class="platz">p13p</td> <td></td> <td></td> <td></td> <td id="22"class="platz"></td> <td id="23"class="platz">p22p</td> <td id="4"class="platz">p4p</td> <td>|</td> </tr> <tr> <td id="14"class="platz">p14p</td> <td id="21"class="platz">p21p</td> <td id="20"class="platz">p20p</td> <td></td> <td></td> <td></td> <td id="3"class="platz">p3p</td> <td>|</td> </tr> <tr> <td id="15"class="platz">p15p</td> <td></td> <td></td> <td></td> <td id="18"class="platz">p18p</td> <td id="19"class="platz">p19p</td> <td id="2"class="platz">p2p</td> <td>|</td> </tr> <tr> <td id="16"class="platz">p16p</td> <td></td> <td></td> <td></td> <td></td> <td></td> <td id="1"class="platz">p1p</td> <td>|</td> </tr> <tr> <td id="17"class="platz">p17p</td> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> <td>|</td> </tr> <tr> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>------</td> <td>|</td> </tr> </table> </div> </body> </html>'
+	for i in array_people:
+		print(i)
+		print(count)
+		page = re.sub('p'+str(count)+'p', i, page)
+		count += 1
+
+	print(array_people)
+
+	pdfkit.from_string(page,'plan.pdf')
+
+
+
+
+array_people = ['Ann Kathrin', 'Ben', 'Charlotte', 'Dana', 'Emily', 'Erik', 'Henrieke', 'Jannis', 'John', 'Jonas', 'Julius', 'Martin', 'Mieke', 'Niklas', 'Niko', 'Ole', 'Oliver', 'Robin', 'Sam', 'Thore', 'Tim Ole', 'Volker']
 array_people_sorted = generateRandomSeating(array_people).randomize()
 
 output_file = open("output.txt","w+")
@@ -64,4 +76,5 @@ for i in range(len(array_people_sorted)):
 	output_file.write(str(i+1)+': '+array_people_sorted[i]+'\n')
 	output = output+str(i+1)+': '+array_people_sorted[i]+'\n'
 
-post_to_whatsapp(output)
+#post_to_whatsapp(output)
+generate_pdf(array_people_sorted)
